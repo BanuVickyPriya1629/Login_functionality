a2c7dc4cbc4c9473fed7b8f45a9847d38c015830
BanVickyPriya1629 <banupriyabalasubramaniam@gmail.com>
2024-01-04 15:37:18 +0530

    loginFunctionality

---
+++ src/test/java/com/Mavenproject9am/Adactin_normal.java
@@ -0,0 +1,29 @@
+package com.Mavenproject9am;
+
+public class Adactin_normal {
+	
+	public static void main(String[] args) {
+		
+		       System.setProperty("webdriver.chrome.driver", "C:\\Users\\ELCOT\\eclipse-workspaceNew\\Selenium_new\\Driver\\chromedriver.exe");
+		       
+		       Webdriver driver = new ChromeDriver();
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+	}
+
+}
